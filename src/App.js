3aaaecbd07005cb589ddf57f8aeba9bc65131484
Naoyuki Ishida <39.ishida@gmail.com>
2023-03-30 13:28:27 -0700

    Article, Content, PublishButton, Titleの各コンポーネントを作成し、エントリポイントに登録

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {Article} from './components/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Article
+        title={'タイトルのサンプール4'}
+        content={'コンテンツのサンプールー4'}
+      />
     </div>
   );
 }
---
+++ src/components/Article.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react';
+import {Content, Title, PublishButton} from './index';
+
+const Article = (props) => {
+  const [isPublished, setIsPublished] = useState(false)
+  const publishArticle = () => {
+    setIsPublished(true)
+  }
+  return (
+    <div className='article'>
+      <Title title={props.title} />
+      <Content content={props.content} />
+      <PublishButton isPublished={isPublished} onClick={() => publishArticle()} />
+    </div>
+  );
+};
+
+export default Article;
